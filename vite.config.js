c5a6d4d9d9bc0410fa564d803b9aefe5a46fdf4f
Хамитов Ильгиз Миндиярович <i.hamitov@sybcom.local>
2026-03-02 15:30:53 +0500

    исправил ошибку fix PWA cache limit

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,21 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate', // Автоматически обновляет приложение у юзера, если ты выкатил новую версию
+      registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      
+      // ВОТ ЭТА НОВАЯ НАСТРОЙКА СПАСЕТ СБОРКУ
+      workbox: {
+        maximumFileSizeToCacheInBytes: 30000000, // Увеличиваем лимит до 30 МБ
+      },
+
       manifest: {
         name: 'AI Stylist Pro',
         short_name: 'Stylist',
         description: 'Ваш умный гардероб и ИИ-стилист в кармане',
         theme_color: '#0b0c10',
         background_color: '#0b0c10',
-        display: 'standalone', // Это убирает рамки браузера!
+        display: 'standalone',
         orientation: 'portrait',
         icons: [
           {
